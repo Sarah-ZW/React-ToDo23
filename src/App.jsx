ca48e4ad3bf168db287e5eafed3fd3690153def2
Sarah-ZW <earthclaninfo@gmail.com>
2023-07-07 22:03:37 -0400

    creating current Todos

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@ import "./styles.css";
 
 export default function App() {
   const [newItem, setNewItem] = useState ("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+       ...currentTodos, 
+        { id:crypto.randomUUID(), title: newItem, completed: false },
+    ]
+  })
+  }
+console.log(todos);
   return ( 
   <> 
-  <form className="new-item-form">
+  <form onSubmit={handleSubmit} className="new-item-form">
   <label htmlFor="item">New Item</label>
   <input value={newItem} type="text" id="item" onChange={e => setNewItem(e.target.value)}></input>
   <button className='btn'>Add</button>
