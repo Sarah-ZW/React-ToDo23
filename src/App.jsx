9ad0c6f222334823a894a19bf0879608e207edda
Sarah-ZW <earthclaninfo@gmail.com>
2023-07-16 21:48:04 -0400

    saving data locally

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles.css";
 import { NewTodoForm } from "./NewTodoForm"
 import { TodoList } from "./ToDoList";
 
 export default function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title){
     setTodos(currentTodos => {
